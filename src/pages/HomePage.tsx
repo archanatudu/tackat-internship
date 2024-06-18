1300fbea44c8ed30b4b656f9af87c25e73787cb9
archanatudu <166447117+archanatudu@users.noreply.github.com>
2024-06-18 18:47:21 +0530

    interface-editor

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-const HomePage:React.FC = () => {
-return (
-    <>
-    <h2>HomePage</h2>
-
-    <button title="students">
-        <Link to="/student/student-details">Student Details</Link>
-    </button>
-
-    <hr />
-    </>
-)
-}
-
-export default HomePage;
-
+  const HomePage:React.FC = () => {
+    return (
+      <><button><Link to ="/student">students</Link></button></>
+    )
+  }
+  export default HomePage;
--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React ,{ useEffect, useState }from "react";
+import { IStudent } from "../interface/student";
+import {Link} from "react-router-dom";
 
 const Students: React.FC = () => {
-return (
-    <div>Students</div>
-)
-}
+    const [allStudents,setAllStudents] = useState<IStudent[]>();
+    useEffect(() => {
+        const students: IStudent[] | undefined = JSON.parse(
+            localStorage.getItem("students") as string
 
-export default Students
+        );
+        console.log(students);
+
+        setAllStudents(students);
+    },[]);
+return (
+    <>
+    
+    <button title=" Create Students">
+        <Link to="/student/create-student">Create Student </Link>
+    </button>
+
+    <br />
+    <hr />
+    <table>
+        <thead>
+        <tr>
+            <td>SN.</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Phone Number</td>
+            <td>Email ID</td>
+        </tr>
+        </thead>
+        <tbody>
+        {allStudents?.map((item, index) => {
+            return (
+            <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+            </tr>
+            );
+        })}
+        </tbody>
+    </table>
+    </>
+);
+};
+
+export default Students;
